c78b38d510757bf444e07841b94d9e5a20c6de02
M3MoN <alan.skrrrz@gmail.com>
2025-04-20 18:45:20 +0200

    feat: add id and link fields to TeamRankingParser for enhanced team data extraction

--- hltv_scraper/hltv_scraper/spiders/parsers/team_ranking.py
+++ hltv_scraper/hltv_scraper/spiders/parsers/team_ranking.py
@@ -6,6 +6,8 @@ class TeamRankingParser(Parser):
         return {
         "position": team.css("span.position::text").get(),
         "name": team.css(".name::text").get(),
+        "id": team.css(".lineup-con .more a.moreLink::attr(href)").re_first(r"/team/(\d+)"),
+        "link": team.css(".lineup-con .more a.moreLink::attr(href)").get(),
         "logo": team.css("span.team-logo img::attr(src)").get(),
         "points": team.css("span.points::text").get(),
         "players": team.css("div.playersLine .rankingNicknames span::text").getall(),
