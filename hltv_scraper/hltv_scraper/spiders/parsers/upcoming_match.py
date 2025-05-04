96c45d0a84a2ef1bedbea47fed13fe90ce248718
M3MONs <alan.skrrrz@gmail.com>
2025-05-04 19:03:08 +0200

    feat: update UpcomingMatchParser to accept date parameter for improved match parsing

--- hltv_scraper/hltv_scraper/spiders/parsers/upcoming_match.py
+++ hltv_scraper/hltv_scraper/spiders/parsers/upcoming_match.py
@@ -4,10 +4,11 @@ from ..utils import is_team_in_upcoming_match
 
 class UpcomingMatchParser(Parser):
     @staticmethod
-    def parse(match):
+    def parse(match, date: str = None):
         if is_team_in_upcoming_match(match):
             return {
                 "hour": match.css("div.match-time::text").get(),
+                "date": date,
                 "meta": match.css("div.match-meta::text").get(),
                 "event": match.css("div.match-event::attr(data-event-headline)").get(),
                 "team1": UMTP.parse(match, 1),
